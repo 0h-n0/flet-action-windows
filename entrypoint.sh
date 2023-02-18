ce5631140f10977454182aacad6860f860b5a545
0h-n0 <kbu94982@gmail.com>
2023-02-18 23:30:37 +0900

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,23 +10,20 @@ set -e
 # Allow the workdir to be set using an env var.
 # Useful for CI pipiles which use docker for their build steps
 # and don't allow that much flexibility to mount volumes
-SRCDIR=$1
-
-ls .
-echo $PWD
+SRCFILE=$1
 
 wine python -m pip install --upgrade pip wheel setuptools
 
 if [ -f "pyproject.toml" ]; then
     echo "LOAD: pyproject.toml"
-    wine poetry install 
-    wine flet pack $SRCDIR
+    wine poetry install --no-dev
+    wine poetry run flet pack $SRCFILE
 elif [ -f "requirements.txt" ]; then
     echo "LOAD: requirements.txt"
     wine python pip install -r ./requirements.txt
-    wine flet pack $SRCDIR
+    wine flet pack $SRCFILE
 else
-    wine flet pack $SRCDIR
+    wine flet pack $SRCFILE
 fi
 
 
